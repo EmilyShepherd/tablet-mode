6cea61d72f9d36c6593d7582577542d47b75493f
Emily Shepherd <emily@emilyshepherd.me>
2016-03-12 00:40:25 +0000

    Removed incorrect behaviour on clicking cancel

--- tablet.sh
+++ tablet.sh
@@ -6,6 +6,11 @@ mode=($( \
     zenity --list --column=A --hide-header "--separator= " \
 ))
 
+if test -z "$mode"
+then
+    exit 0
+fi
+
 # If we've selected a tablet mode, replace the physical keyboard with an
 # onscreen one
 if ! test "${mode[0]}" == "Normal"
