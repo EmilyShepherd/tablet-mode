a89f9a3a179a4e498258ffcdbd1e4205b6e2963e
Emily Shepherd <emily@emilyshepherd.me>
2016-03-12 00:33:30 +0000

    Comments and code neatness

--- tablet.sh
+++ tablet.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-mode=($(printf "Normal\nLeft\nRight\nInverted\n" | \
-    zenity --list --column=A --hide-header "--separator= "))
+# Ask the user which tablet mode to go into
+mode=($( \
+    printf "Normal\nLeft\nRight\nInverted\n" | \
+    zenity --list --column=A --hide-header "--separator= " \
+))
 
+# If we've selected a tablet mode, replace the physical keyboard with an
+# onscreen one
 if ! test "${mode[0]}" == "Normal"
 then
     xinput disable "AT Translated Set 2 keyboard"
     onboard &
+# Close the onscreen keyboard and re-enabled the physical one
 else
     xinput enable "AT Translated Set 2 keyboard"
     killall onboard
 fi
 
+# Actually do the flip of the screen, with a lower case version of the
+# orientation
 xrandr --output eDP1 --rotate $(echo ${mode[0]} | tr '[:upper:]' '[:lower:]')
